8b33a0bc82557fdb711f5b6220a7298631d4e8b3
your_name <your_address_email@example.com>
2023-04-22 23:12:31 +0700

    First commit checkbox problem using useReducer hook

--- src/App.js
+++ src/App.js
@@ -1,24 +1,71 @@
-import logo from './logo.svg';
+import {useReducer} from 'react';
+import { React } from 'react';
 import './App.css';
+const initialBoxList = [
+  {
+    id: 1,
+    content: "Todo item 1",
+    status: false
+  },
+  {
+    id: 2,
+    content: "Todo item 2",
+    status: true
+  }
+];
 
+const reducer = (items, action) => {
+  if (action.type == "COMPLETE") {
+    return items.map(item => {
+      if (item.id == action.id) {
+        return {
+          ...item,
+          status: !item.status
+        }
+      }
+      else {
+        return item;
+      }
+    })
+  }
+  else {
+    return items;
+  }
+}
 function App() {
+  const [items, dispatch] = useReducer(reducer, initialBoxList);
+  const handleChecked = item => {
+    dispatch({
+      type: "COMPLETE",
+      id: item.id
+    })
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <div>
+      <h2
+        style={{
+          fontWeight: "bold",
+          color: "red",
+          textAlign: "center"
+        }}
+      >
+        This project for testing useReducer hook by using checkbox problem
+      </h2>
+      <ul
+        style={{listStyleType: "none"}}
+      >
+          {items.map(item => (
+            <li key={item.id}>
+              <input 
+                type="checkbox"
+                checked={item.status}
+                onChange ={() => handleChecked(item)} 
+              />
+              {item.content}
+            </li>
+          ))}
+      </ul>
+   </div>
   );
 }
 
